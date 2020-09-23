89367f3f559a687ad1d7f11f80a368e9a4a7fc13
Stanley Lim <slim679975@gmail.com>
2020-09-22 23:29:45 -0700

    :sparkles: feat(test.py): Updated test script

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 from threading import Thread
 import time
 import requests
+import string
+import random
 
 def increment_number(name, delay):
     count = 0
@@ -9,6 +11,16 @@ def increment_number(name, delay):
 
         count = requests.get('http://localhost:8080/increment')
         print(f'{name} - {count.text}')
+    
+def add_string(name, delay):
+    words = ''
+    for i in range(10):
+        time.sleep(delay)
+
+        requested_word = _get_random_string(5)
+        print(f'{name} - Requesting with {requested_word}')
+        words = requests.post('http://localhost:8080/addstring', data=requested_word)
+        print(f'{name} - {words.text}')
 
 def _reset():
     requests.post('http://localhost:8080/reset')
@@ -17,11 +29,12 @@ def _get_random_string(length):
     letters = string.ascii_lowercase
     return ''.join(random.choice(letters) for i in range(length))
 
+_reset()
 THREAD_COUNT = 4
 threads = []
 try:
     for i in range(THREAD_COUNT):
-        t = Thread(target=increment_number, args=(f'Thread {i}', 0.05))
+        t = Thread(target=add_string, args=(f'Thread {i}', 0.01))
         t.start()
         threads.append(t)
 except:
